b59b8c7758a65144b2f2e1f92d9c08e2e5d47030
tigranyeghyan <tigran.yeghyan.y.2004@gmail.com>
2024-11-13 16:22:43 +0400

    237. Delete Node in a Linked List

---
+++ 237/task.cpp
@@ -0,0 +1,18 @@
+struct ListNode 
+{
+      int val;
+      ListNode *next;
+      ListNode(int x) : val(x), next(nullptr) {}
+};
+
+
+class Solution {
+public:
+    void deleteNode(ListNode* node) 
+    {
+        ListNode* tmp = node -> next;
+        node -> val =  node -> next -> val; 
+        node -> next = node -> next -> next;
+        delete tmp; 
+    }
+};
